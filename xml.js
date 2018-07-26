4b5c5e47fe8c7402d35f6f8a94e9e88198d8e7e9
Diego Ceraso <dceraso@smartqube.com.ar>
2018-07-26 00:46:18 -0300

    Modificacion para guardar xml naturales en storage

--- xml.js
+++ xml.js
@@ -2,7 +2,13 @@ var express = require('express'),
     typeis = require('type-is'),
     parser = require('xml2json'),
     bodyParser = require('body-parser'),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    fs = require('fs'),
+    path = require('path');
+
+const foldersmq = 'smq/';
+const folderandreani = 'andreani/';
+const dirpath = path.join(__dirname, 'files_xmls/');
 
 var app = express();
 
@@ -21,7 +27,7 @@ var xmlParseOptions = {
 
 app.use(bodyParser.text({
   type: '*/xml',
-  limit: '1MB'
+  limit: '5MB'
 }));
 
 app.use(function(req, res, next) {
@@ -52,38 +58,81 @@ var nameSchema = new mongoose.Schema({ any : {}  }, {strict : false});
 //Modelo SMQ
 var eventosdn = mongoose.model("evento", nameSchema);
 
-
+var eventosdnandreani = mongoose.model("eventoandreani", nameSchema);
 
 // XML Response Route
 app.post('/sdn/save', function (req, res) {
-    console.log(req.body);
-    console.log("=================================================================");
-    console.log("=================================================================");
+    //console.log(req.body);
+    //console.log("=================================================================");
+    //console.log("=================================================================");
     var options = {
         object: true,
         alternateTextNode: true
      };
     var json = parser.toJson(req.body, options);
-    console.log(typeof json);
-    console.log("REQ.BODY to json -> %s", json);
+    //console.log(typeof json);
+    //console.log("REQ.BODY to json -> %s", json);
     var eventoObjeto = new eventosdn({evento : json});
-    console.log("=================================================================");
-    console.log("=================================================================");
-    console.log("JSON to objetomongo -> %s", eventoObjeto);
-    console.log("=================================================================");
-    console.log("=================================================================");
-    eventoObjeto.save()
-        .then(item => {
-            console.log("Name saved to database");
-        })
-        .catch(err => {
-            console.log("Unable to save to database");
-        });
+    //console.log("=================================================================");
+    //console.log("=================================================================");
+    //console.log("JSON to objetomongo -> %s", eventoObjeto);
+    //console.log("=================================================================");
+    //console.log("=================================================================");
+    eventoObjeto.save((result, err) =>{
+        if(!err) {
+            const rutafinal = (dirpath+foldersmq);
+            fs.writeFile(rutafinal+"error_"+Date()+".txt", req.body+"\r\n"+"----------"+"\r\n"+eventoObjeto)
+          }
+
+    }).then((result)=>{
+        const rutafinal = (dirpath+foldersmq+result["_id"]+".xml");
+        //console.log("El path final es:"+ rutafinal);
+        fs.writeFile(rutafinal, req.body)
+
+    }).catch(err => {
+        console.log(err);
+     });
+    
+    
+})
+
+
+app.post('/andreani/sdn/save', function (req, res) {
+    //console.log(req.body);
+    //console.log("=================================================================");
+    //console.log("=================================================================");
+    var options = {
+        object: true,
+        alternateTextNode: true
+     };
+    var json = parser.toJson(req.body, options);
+    //console.log(typeof json);
+    //console.log("REQ.BODY to json -> %s", json);
+    var eventoObjeto = new eventosdnandreani({evento : json});
+    //console.log("=================================================================");
+    //console.log("=================================================================");
+    //console.log("JSON to objetomongo -> %s", eventoObjeto);
+    //console.log("=================================================================");
+    //console.log("=================================================================");
+    eventoObjeto.save((result, err) =>{
+        if(!err) {
+            const rutafinal = (dirpath+folderandreani);
+            fs.writeFile(rutafinal+"error_"+Date()+".txt", req.body+"\r\n"+"----------"+"\r\n"+eventoObjeto)
+          }
+
+    }).then((result)=>{
+        const rutafinal = (dirpath+folderandreani+result["_id"]+".xml");
+        //console.log("El path final es:"+ rutafinal);
+        fs.writeFile(rutafinal, req.body)
+
+    }).catch(err => {
+        console.log(err);
+     });
     
     
 })
 
 //.. Run Server
 let server = app.listen(3000, function () {
-    console.log('API running at', server.address().port);
+    //console.log('API running at', server.address().port);
 });
